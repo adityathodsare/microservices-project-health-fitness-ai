e0bba8112617f1191c97ae8b3f233cd858a77cc4
adityathodsare <thodsareaditya@gmail.com>
2025-07-08 17:20:42 +0530

    config server 1

--- config-server/config-server/src/main/java/com/health_config_server/config_server/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/com/health_config_server/config_server/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.health_config_server.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ConfigServerApplication {
 
